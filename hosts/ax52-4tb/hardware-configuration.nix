0e92c24bc05410c3cf1cf957104c8567bfa1e6c2
will <will@256k1.dev>
2025-04-11 10:59:00 +0100

    use normal fs for ax52-4tb

--- hosts/ax52-4tb/hardware-configuration.nix
+++ hosts/ax52-4tb/hardware-configuration.nix
@@ -1,83 +1,91 @@
-{ modulesPath, ... }: {
+{ lib, modulesPath, ... }: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
   disko.devices = {
-    disk = {
-      x = {
-        type = "disk";
-        device = "/dev/nvme0n1";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              size = "64M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
-            };
-            zfs = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "zroot";
-              };
+    disk.disk1 = {
+      device = lib.mkDefault "/dev/nvme2n1";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          boot = {
+            name = "boot";
+            size = "1M";
+            type = "EF02";
+          };
+          esp = {
+            name = "ESP";
+            size = "500M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
             };
           };
-        };
-      };
-      y = {
-        type = "disk";
-        device = "/dev/nvme1n1";
-        content = {
-          type = "gpt";
-          partitions = {
-            zfs = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "zroot";
-              };
-            };
-          };
-        };
-      };
-      z = {
-        type = "disk";
-        device = "/dev/nvme2n1";
-        content = {
-          type = "gpt";
-          partitions = {
-            zfs = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "zroot";
-              };
+          root = {
+            name = "root";
+            size = "100%";
+            content = {
+              type = "lvm_pv";
+              vg = "pool";
             };
           };
         };
       };
     };
-    zpool = {
-      zroot = {
-        type = "zpool";
-        mode = "stripe";
-        options.cachefile = "none";
-        rootFsOptions = {
-          compression = "zstd";
-          "com.sun:auto-snapshot" = "false";
+    disk.disk2 = {
+      device = lib.mkDefault "/dev/nvme0n1";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          data = {
+            name = "data";
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/data1";
+              mountOptions = [ "defaults" ];
+            };
+          };
         };
-        mountpoint = "/";
-        datasets = {
-          zfs_fs = {
-            type = "zfs_fs";
-            mountpoint = "/";
+      };
+    };
+    disk.disk3 = {
+      device = lib.mkDefault "/dev/nvme1n1";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          data = {
+            name = "data";
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/data2";
+              mountOptions = [ "defaults" ];
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%FREE";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = [ "defaults" ];
+            };
           };
         };
       };
