d28c883bba62e6baaa9ac204246a1127e32a564a
will <will@256k1.dev>
2025-04-11 12:09:03 +0100

    lock down bootloader a little

--- hosts/ax52-4tb/hardware-configuration.nix
+++ hosts/ax52-4tb/hardware-configuration.nix
@@ -22,7 +22,7 @@
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
-              mountOptions = [ "defaults" ];
+              mountOptions = [ "defaults" "umask=0077" ];
             };
           };
           root = {
