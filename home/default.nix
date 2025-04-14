e28dd86696f53a6ec48543bbe2fcabef81a16e9a
will <will@256k1.dev>
2025-04-14 14:40:02 +0100

    add justfile to bitcoin

--- home/default.nix
+++ home/default.nix
@@ -5,6 +5,7 @@
     "d /home/${deployment.username}/setup - ${deployment.username} users - -"
     "d /home/${deployment.username}/setup/bitcoin - ${deployment.username} users - -"
     "C /home/${deployment.username}/setup/bitcoin/shell.nix - ${deployment.username} users - ${./bitcoin/shell.nix}"
+    "C /home/${deployment.username}/setup/bitcoin/justfile - ${deployment.username} users - ${./bitcoin/justfile}"
     "d /home/${deployment.username}/setup/env - ${deployment.username} users - -"
     "C /home/${deployment.username}/setup/env/.envrc - ${deployment.username} users - ${./env/.envrc}"
     "C /home/${deployment.username}/setup/justfile - ${deployment.username} users - ${./justfile}"
