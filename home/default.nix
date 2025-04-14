cc5b5d040f4ee927bdbe1116ce336268d0b538dc
will <will@256k1.dev>
2025-04-14 14:38:26 +0100

    revert to shell.nix

--- home/default.nix
+++ home/default.nix
@@ -4,7 +4,8 @@
   systemd.tmpfiles.rules = [
     "d /home/${deployment.username}/setup - ${deployment.username} users - -"
     "d /home/${deployment.username}/setup/bitcoin - ${deployment.username} users - -"
-    "C /home/${deployment.username}/setup/bitcoin/flake.nix - ${deployment.username} users - ${./bitcoin/flake.nix}"
+    "C /home/${deployment.username}/setup/bitcoin/shell.nix - ${deployment.username} users - ${./bitcoin/shell.nix}"
+    "d /home/${deployment.username}/setup/env - ${deployment.username} users - -"
     "C /home/${deployment.username}/setup/env/.envrc - ${deployment.username} users - ${./env/.envrc}"
     "C /home/${deployment.username}/setup/justfile - ${deployment.username} users - ${./justfile}"
   ];
