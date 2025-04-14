f08c25fee2e20937887cf2c525e5a709b68a64c3
will <will@256k1.dev>
2025-04-14 14:24:15 +0100

    fixup home flake

--- home/default.nix
+++ home/default.nix
@@ -2,10 +2,11 @@
 
   # Create the setup directory and copy justfile and bitcoin directory
   systemd.tmpfiles.rules = [
-    "d /home/${deployment.username}/setup - ${deployment.username} ${deployment.username} - -"
-    "C /home/${deployment.username}/setup/.envrc - ${deployment.username} ${deployment.username} - ${./justfile}"
-    "C /home/${deployment.username}/setup/bitcoin - ${deployment.username} ${deployment.username} - ${./bitcoin}"
-    "C /home/${deployment.username}/setup/justfile - ${deployment.username} ${deployment.username} - ${./justfile}"
+    "d /home/${deployment.username}/setup - ${deployment.username} users - -"
+    "d /home/${deployment.username}/setup/bitcoin - ${deployment.username} users - -"
+    "C /home/${deployment.username}/setup/bitcoin/flake.nix - ${deployment.username} users - ${./bitcoin/flake.nix}"
+    "C /home/${deployment.username}/setup/env/.envrc - ${deployment.username} users - ${./env/.envrc}"
+    "C /home/${deployment.username}/setup/justfile - ${deployment.username} users - ${./justfile}"
   ];
 
 }
